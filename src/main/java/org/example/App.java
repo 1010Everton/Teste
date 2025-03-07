833378ee231d23e752fa5d90ded61140f6554ce8
EvErToN <evertonflorenciodasilva@outlook.com>
2025-03-07 10:21:04 -0300

    treinando o if, criei uma classe para chamar o scanner estou fazendo a logica para digitar e receber o numero da nota

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,28 @@
 package org.example;
 
 /**
- * Hello world!
+ *  1 - Peça ao usuário para inserir uma nota (valor numérico entre 0 e 100).
+ *
+ * Avalie a nota usando a estrutura de controle if-else e determine a classificação da nota de acordo com a seguinte tabela:
+ *
+ * Nota >= 90: Excelente
+ *
+ * Nota >= 80 e < 90: Muito bom
+ *
+ * Nota >= 70 e < 80: Bom
+ *
+ * Nota >= 60 e < 70: Satisfatório
+ *
+ * Nota < 60: Insuficiente
  *
  */
 public class App 
 {
+    protected byte numero;
+
+    public App (byte numero){
+        this.numero = numero;
+
+    }
 
 }
---
+++ src/main/java/org/example/Leitura.java
@@ -0,0 +1,7 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Leitura {
+    Scanner scan = new Scanner(System.in);
+}
